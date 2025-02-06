555072c49fe5355d7901fc91851542f084386eda
joshmasterton <joshmasterton@tuta.io>
2025-02-06 13:41:22 +0000

    Update database.config.ts

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -1,19 +1,25 @@
 import { adminPoll } from "./pool.config";
 
-const { POSTGRES_DB } = process.env;
+const { POSTGRES_URL } = process.env;
 
 export const createDatabaseIfNotExists = async () => {
   const client = await adminPoll.connect();
 
+  if (!POSTGRES_URL) {
+    throw new Error("DATABASE_URL could not be found");
+  }
+
+  const databaseName = new URL(POSTGRES_URL).pathname.split("/")[1];
+
   const response = await client.query(
     `SELECT 1 FROM pg_database WHERE datname = $1`,
-    [POSTGRES_DB]
+    [POSTGRES_URL]
   );
 
   if (response.rowCount === 0) {
-    await client.query(`CREATE DATABASE ${POSTGRES_DB}`);
+    await client.query(`CREATE DATABASE ${databaseName}`);
   } else {
-    console.log(`Database ${POSTGRES_DB} already exists`);
+    console.log(`Database ${databaseName} already exists`);
   }
   try {
   } catch (error) {
